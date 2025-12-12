1ba68cbf13f19fa090760aa6c173e8f7e0fc98d6
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-12 10:41:50 +0100

    Create LOOM_GLIMPSE2_all.sh

---
+++ generate_each_datatype/low-pass_imputed/LOOM_GLIMPSE2_all.sh
@@ -0,0 +1,15 @@
+### LOOM LOW-PASS FULL PIPELINE ###
+
+samples="/path/to/data/GeT-RM_low_pass_CRAM/get-rm_samples.txt"
+nb_samples=$(wc -l $samples | awk '{print $1}')
+tmp="/bigtmp/"
+cram_path="/path/to/data/GeT-RM_low_pass_CRAM/"
+ref_path="/path/to/data/"
+header=${cram_path}header.txt
+
+seq 1 $nb_samples | xargs -I {} -P 1 bash -c '
+    sample=$(sed -n '\''{}p'\'' '"${samples}"')
+    echo ${sample}
+    ./LOOM_GLIMPSE2_unit.sh ${sample}'
+
+./LOOM_GLIMPSE2_reheader.sh $cram_path $tmp $samples $header
