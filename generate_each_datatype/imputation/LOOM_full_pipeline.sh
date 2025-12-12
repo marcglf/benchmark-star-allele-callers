0b301335772eb158931df3665d60d4e388a62a86
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-12 10:26:43 +0100

    Update LOOM_full_pipeline.sh

--- generate_each_datatype/imputation/LOOM_full_pipeline.sh
+++ generate_each_datatype/imputation/LOOM_full_pipeline.sh
@@ -1,1 +1,15 @@
 
+##### LOOM FULL PIPELINE #####
+
+### CONFIG ###
+path=/path/to/data/Genotyping/
+out=/path/to/data/imputation/
+tmp=/bigtmp/mgroslafaige/1KG/
+minimac_in=/path/to/data/imputation/
+samples=/path/to/data/imputation/get-rm_samples.txt
+header=/path/to/data/imputation/header.txt
+
+### MAIN SCRIPT ###
+
+./All_1KG_imputation.sh $path $out $tmp $minimac_in $samples
+./reheader.sh $path $out $tmp $minimac_in $samples $header
