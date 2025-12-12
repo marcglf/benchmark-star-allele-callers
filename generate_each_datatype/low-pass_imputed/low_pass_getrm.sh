a7a0a04440d5d49f6519ecc2641015abb85d83ac
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-12 10:36:18 +0100

    Create low_pass_getrm.sh

---
+++ generate_each_datatype/low-pass_imputed/low_pass_getrm.sh
@@ -0,0 +1,25 @@
+### Marc GLF ###
+### low-pass on 1KG | GeT-RM sample (0.5x) ###
+
+samples="/path/to/data/GeT-RM_low_pass_CRAM/get-rm_samples.txt"
+nb_samples=$(wc -l $samples | awk '{print $1}')
+tmp="/bigtmp/"
+cram_path="/path/to/data/GeT-RM_low_pass_CRAM/"
+ref_path="/path/to/data/"
+
+
+seq 1 $nb_samples | xargs -I {} -P 11 bash -c '
+    ID=$(sed -n '{}p' '"${samples}"')
+    link=$(cat /path/to/data/CRAM/1KG_cram_list.txt |grep ${ID})
+    echo downloading $ID
+    wget $link
+    samtools view -s 42.0167 -C -o ${ID}.low-pass.0.5x.cram ${ID}.final.cram
+    rm ${ID}.final.cram'
+
+mv /bigtmp/* /path/to/data/GeT-RM_low_pass_CRAM/
+
+cd /path/to/data/GeT-RM_low_pass_CRAM/
+
+samtools merge -O CRAM -o merge.low-pass.0.5x.cram -@20 *.low-pass.0.5x.cram
+
+samtools index merge.low-pass.0.5x.cram
