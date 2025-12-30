10826cdd94ac032b05a5c76bc0078be1aa4ece83
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-30 15:27:58 +0100

    Create PyPGx_control_statistics.sh

---
+++ running_each_pipeline/PyPGx_control_statistics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# filename
+fichier="1KG_cram_list.txt"
+
+# read each line
+while IFS= read -r ligne; do
+    echo "Ligne : $ligne"
+    ID=$(echo "$ligne" | sed 's|.*/||; s|\..*||')
+    pypgx compute-control-statistics RYR1 1KG-control-statistics_$ID.zip $ligne --assembly GRCh38
+done < "$fichier"
