2b906fc674ea8a996e332fdd94e8d3fc82a079d4
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-30 15:25:19 +0100

    Create PyPGx_depth_of_coverage.sh

---
+++ running_each_pipeline/PyPGx_depth_of_coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# filename
+fichier="1KG_cram_list.txt"
+
+# read each line 
+while IFS= read -r ligne; do
+    echo "Ligne : $ligne"
+    ID=$(echo "$ligne" | sed 's|.*/||; s|\..*||')
+    pypgx prepare-depth-of-coverage 1KG-depth-of-coverage_$ID.zip $ligne --assembly GRCh38
+done < "$fichier"
