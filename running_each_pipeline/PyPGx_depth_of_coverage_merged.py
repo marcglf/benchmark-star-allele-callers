a5618bc163141e211dc0aa43adcab2b0f000da53
marcglf <151172658+marcglf@users.noreply.github.com>
2025-12-30 15:26:10 +0100

    Create PyPGx_depth_of_coverage_merged.py

---
+++ running_each_pipeline/PyPGx_depth_of_coverage_merged.py
@@ -0,0 +1,18 @@
+import pypgx # type: ignore
+import os
+import pandas as pd # type: ignore
+import fuc #type: ignore
+
+l = [i for i in os.listdir() if ".zip" in i and "depth-of-coverage_" in i]
+
+data = pd.DataFrame()
+for i in l:
+    t = pypgx.sdk.Archive.from_file(i)
+    if i == l[0]:
+        data = t.data.df
+    else :
+        data = pd.merge(data, t.data.df, on=['Chromosome', 'Position'])
+
+data = fuc.api.pycov.CovFrame(data)
+metadata = t.metadata
+result = pypgx.sdk.Archive(metadata, data)
